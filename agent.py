31eafa7ccd6002fc1145532d951792845e7b343d
Mohit Malviya <mohitmalviya0107@gmail.com>
2026-04-16 11:38:20 +0530

    Update agent.py

--- agent.py
+++ agent.py
@@ -4,7 +4,7 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from tools import web_search , scrape_url 
 from dotenv import load_dotenv
-
+  
 load_dotenv()
 
 #model setup 
